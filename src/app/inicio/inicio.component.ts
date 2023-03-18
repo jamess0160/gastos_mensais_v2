b6ea5c4f58c6ff0280f20cc873e918ee605fa083
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-03-18 18:26:47 -0300

    cria componente para tela inicial

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.scss']
+})
+export class InicioComponent {
+  bancos = [
+    {
+      id: 1,
+      nome: "",
+      cor: "#8A05BE",
+      totais: {
+        alimentacao: 500,
+        transportes: 500,
+        geral: 500,
+      }
+    },
+    {
+      id: 1,
+      nome: "Teste",
+      cor: "#8A05BE",
+      totais: {
+        alimentacao: 500,
+        transportes: 500,
+        geral: 500,
+      }
+    },
+    {
+      id: 1,
+      nome: "Teste",
+      cor: "#8A05BE",
+      totais: {
+        alimentacao: 500,
+        transportes: 500,
+        geral: 500,
+      }
+    }
+  ]
+}
