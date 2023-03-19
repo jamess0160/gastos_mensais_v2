80ea50aa8056e600d5799467d98146a4487a1333
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-03-19 14:37:20 -0300

    add rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "",
+		redirectTo: "inicio",
+		pathMatch: "full"
+	},
+	{
+		path: "inicio",
+		component: InicioComponent
+	},
+	{
+		path: "categorias/:banco",
+		component: CategoriasComponent
+	}
+]; 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
