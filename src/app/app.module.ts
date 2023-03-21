f60d5563400767e7af8004ce697efb66d8c02b80
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-03-21 18:21:49 -0300

    remove sistema de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,28 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ConfiguracoesComponent } from './configuracoes/configuracoes.component';
 import { VoltarComponent } from './voltar/voltar.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { GastoService } from './gastos';
+import { EntradaService } from './entradas';
+import { BancoService } from './bancos';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     ConfiguracoesComponent,
-    VoltarComponent
+    VoltarComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    GastoService,
+    EntradaService,
+    BancoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
