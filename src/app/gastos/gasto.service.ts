4a099e76e492ea0090eac6ab047efcc4af362cdc
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-03-22 13:00:15 -0300

    add tratativa de erros e loop para letra primeira maiuscula

--- src/app/gastos/gasto.service.ts
+++ src/app/gastos/gasto.service.ts
@@ -10,20 +10,44 @@ export class GastoService {
 
 	constructor() { }
 
-	async listarGastosMes(mes: number = new Date().getMonth()): Promise<Gasto[]> {
-		let { data } = await axios.get(`${urlApi}/registro_gastos/${mes}`)
-		return data
+	async listarGastos(banco: number, tipo: number, mes: number = new Date().getMonth() + 1): Promise<Gasto[]> {
+		try {
+			let { data }: { data: Gasto[] } = await axios.get(`${urlApi}/registro_gastos/${banco}/${tipo}/${mes}`)
+			data.forEach((item) => {
+				item.data_gasto = item.data_gasto ? new Date(item.data_gasto).toLocaleDateString('pt-br') : ""
+				if (item.descricao) {
+					let letras = item.descricao.split("")
+					letras[0] = letras[0].toUpperCase()
+					item.descricao = letras.join("")
+				}
+			})
+			return data
+		} catch (error) {
+			return []
+		}
 	}
 
 	async inserirGasto(gasto: Gasto): Promise<void> {
-		await axios.post(`${urlApi}/registro_gastos/`, gasto)
+		try {
+			await axios.post(`${urlApi}/registro_gastos/`, gasto)
+		} catch (error) {
+			alert("Aconteceu um erro ao inserir")
+		}
 	}
 
 	async atualizarGasto(dadosNovos: Gasto, id: number): Promise<void> {
-		await axios.put(`${urlApi}/registro_gastos/${id}`, dadosNovos)
+		try {
+			await axios.put(`${urlApi}/registro_gastos/${id}`, dadosNovos)
+		} catch (error) {
+			alert("Aconteceu um erro ao atualizar")
+		}
 	}
 
 	async deletarGasto(id: number): Promise<void> {
-		await axios.delete(`${urlApi}/registro_gastos/${id}`)
+		try {
+			await axios.delete(`${urlApi}/registro_gastos/${id}`)
+		} catch (error) {
+			alert("Aconteceu um erro ao deletar")
+		}
 	}
 }
