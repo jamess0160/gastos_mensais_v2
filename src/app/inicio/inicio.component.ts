1be24ea310b433c33795b77d3fe098e92f5db440
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-03-23 13:44:09 -0300

    finalização das funcionalidades da tela inicial

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,46 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BancoService } from '../bancos';
+import { Tile } from '../bancos/banco.model';
+import { EntradaService } from '../entradas';
+import utils from '../utils';
 
 @Component({
     selector: 'app-inicio',
     templateUrl: './inicio.component.html',
     styleUrls: ['./inicio.component.scss']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+
+    constructor(private BancoService: BancoService, private EntradaService: EntradaService) { }
 
     dialogOpen = false
 
-    bancos = [
-        {
-            id: 1,
-            nome: "",
-            cor: "#8A05BE",
-            totais: {
-                alimentacao: 500,
-                transportes: 500,
-                geral: 500,
-            }
-        },
-        {
-            id: 2,
-            nome: "Teste",
-            cor: "#8A05BE",
-            totais: {
-                alimentacao: 500,
-                transportes: 500,
-                geral: 500,
-            }
-        },
-        {
-            id: 3,
-            nome: "Teste",
-            cor: "#8A05BE",
-            totais: {
-                alimentacao: 500,
-                transportes: 500,
-                geral: 500,
-            }
-        }
-    ]
+    bancos: Tile[] = []
+    totalGastos: string = ""
+    totalEntradas: string = ""
+    restante: string = ""
+
+    ngOnInit() {
+        utils.callInterval(this.carregarDados, 2000, "Ocorreu um erro ao carregar os dados", this)
+    }
+
+    async carregarDados() {
+        let dadosBancos = await this.BancoService.listarGastoPorBancos()
+        let entradas = await this.EntradaService.listarEntradasMes()
+
+        this.totalGastos = dadosBancos.reduce((anterior, atual) => anterior + atual.total, 0).toFixed(2)
+        this.totalEntradas = entradas.reduce((anterior, atual) => anterior + atual.valor, 0).toFixed(2)
+        this.restante = (parseInt(this.totalEntradas) - parseInt(this.totalGastos)).toFixed(2)
+
+        this.bancos = [
+            {
+                id: 0,
+                nome: "",
+                total: 0,
+                totais: {
+                    geral: dadosBancos.reduce((anterior, atual) => anterior + parseInt(atual.totais.geral), 0).toFixed(2),
+                    transportes: dadosBancos.reduce((anterior, atual) => anterior + parseInt(atual.totais.transportes), 0).toFixed(2),
+                    alimentacao: dadosBancos.reduce((anterior, atual) => anterior + parseInt(atual.totais.alimentacao), 0).toFixed(2)
+                }
+            },
+            ...dadosBancos
+        ]
+    }
 
     abrirAdicionar() {
         this.dialogOpen = true
